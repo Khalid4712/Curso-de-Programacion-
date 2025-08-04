8058e7bf1376603f5e240308520adaa357cc8785
Khalid4712 <darwinvillalobos0201@gmail.com>
2025-08-04 11:25:27 -0400

    Add files via upload

---
+++ ejercicio1(arrays).php
@@ -0,0 +1,17 @@
+<?php
+  $frutas = ["Manzana", "Pera", "Banana", "Uva", "Naranja"];
+  echo "El elemento en la posición 1 es: " . $frutas[0];
+  echo "\nEl elemento en la posición 2 es: " . $frutas[1];
+  echo "\nEl elemento en la posición 3 es: " . $frutas[2];
+  echo "\nEl elemento en la posición 1 es: " . $frutas[3];
+  echo "\nEl elemento en la posición 1 es: " . $frutas[4];
+  
+  $producto = [
+    "nombre" => "Telefono",
+    "precio" => "89.99$",
+    "stock" => "25"
+ ];
+ echo "\n\nEl nombre del producto es: ". $producto["nombre"];
+ echo "\nEl precio del producto es: ". $producto["precio"];
+ echo "\nEl stock del producto es: ". $producto["stock"];
+?>
