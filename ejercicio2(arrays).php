c189d5adf139f0db2b31cd8236bafa42edefa3e8
Khalid4712 <darwinvillalobos0201@gmail.com>
2025-08-04 11:26:45 -0400

    Update ejercicio2(arrays).php

---
+++ ejercicio2(arrays).php
@@ -0,0 +1,13 @@
+<?php
+$estudiantes = [
+    [ "nombre" => "Khalid", "nota" => 95 ],
+    [ "nombre" => "Klaussen", "nota" => 82 ],
+    [ "nombre" => "Dooksthersz", "nota" => 100 ]
+];
+
+echo "La nota de Khalid (estudiante en índice 1) es: " . $estudiantes[0]["nota"] . "\n";
+
+echo "La nota de Klaussen (estudiante en índice 1) es: " . $estudiantes[1]["nota"] . "\n";
+
+echo "La nota de Dooksthersz (estudiante en índice 1) es: " . $estudiantes[2]["nota"] . "\n";
+?>
