fb5adbf026b27b1302a6b5e88b780691165997f0
Khalid4712 <darwinvillalobos0201@gmail.com>
2025-08-06 11:54:48 -0400

    Add files via upload

--- calculadorac.cpp
+++ calculadorac.cpp
@@ -1,9 +1,11 @@
 #include <iostream>
 #include <cmath>
 
-int main(){
+int main() {
          
-    int opcion, num1, num2, suma, resta, multiplicacion, division, raiz_cuadrada, modulo, cociente;
+    int opcion, num1, num2, suma, resta, multiplicacion, division, modulo, cociente;
+    float raiz_cuadrada;
+    
     std::cout<<"1. Suma\n2. Resta\n3. Multipicacion\n4. Division\n5. Raiz cuadrada\n6. Modulo\n7. Cociente\n8. Seno\n9. Coseno\n10.Tangente\n\nElige una opcion: ";
     std::cin>>opcion;
 
@@ -11,13 +13,14 @@ int main(){
         case 1:
         std::cout<<"Ingresa el primer numero: ";
         std::cin>>num1;
-
+        
         std::cout<<"Ingresa el segundo numero: ";
         std::cin>>num2;
 
         suma = num1+num2;
 
         std::cout<<"El resultado es: "<<suma;
+        break;
 
         case 2:
         std::cout<<"Ingresa el primer numero: ";
@@ -29,6 +32,7 @@ int main(){
         resta = num1-num2;
 
         std::cout<<"El resultado es: "<<resta;
+        break;
 
         case 3:
         std::cout<<"Ingresa el primer numero: ";
@@ -40,6 +44,7 @@ int main(){
         multiplicacion = num1*num2;
 
         std::cout<<"El resultado es: "<<multiplicacion;
+        break;
 
         case 4:
         std::cout<<"Ingresa el primer numero: ";
@@ -48,9 +53,14 @@ int main(){
         std::cout<<"Ingresa el segundo numero: ";
         std::cin>>num2;
 
+        if (num2!=0){
         division = num1/num2;
-
         std::cout<<"El resultado es: "<<division;
+        }
+        else {
+            std::cout<<"Division no permitida";
+        }
+        break;
 
         case 5:
         std::cout<<"Ingresa el numero: ";
@@ -59,6 +69,7 @@ int main(){
         raiz_cuadrada = sqrt(num1);
 
         std::cout<<"El resultado es: "<<raiz_cuadrada;
+        break;
 
         case 6:
         std::cout<<"Ingresa el primer numero: ";
@@ -70,6 +81,7 @@ int main(){
         modulo = num1%num2;
 
         std::cout<<"El resultado es: "<<modulo;
+        break;
 
         case 7:
         std::cout<<"Ingresa el dividendo: ";
@@ -81,6 +93,7 @@ int main(){
         cociente = num1/num2;
 
         std::cout<<"El resultado es: "<<cociente;
+        break;
         
         case 8: 
         std::cout << "Ingresa un numero (en radianes): ";
@@ -102,6 +115,7 @@ int main(){
          
         default:
         std::cout<<"Operador no valido, ingresa uno de los disponibles";
+        break;
     }
 
     return 0;
